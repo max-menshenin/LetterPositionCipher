78d02d0b578cf89a02f0a3af323e0675200b2f2b
postmind <postmind24@gmail.com>
2020-06-04 19:01:01 +0300

    remastered

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-cyrrilyc_alpha = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+CYRRILYC_ALPHA = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 text = 'варенник'
 
-cyrrilyc_alpha_dict = dict(enumerate(cyrrilyc_alpha, 1))
-cyrrilyc_alpha_dict = {v: k for k, v in cyrrilyc_alpha_dict.items()}
+CYRRILYC_ALPHA_DICT = dict(enumerate(CYRRILYC_ALPHA, 1))
+CYRRILYC_ALPHA_DICT = {v: k for k, v in CYRRILYC_ALPHA_DICT.items()}
 
-result = ''
+result = ' '
 for c in text:
-    result += str(cyrrilyc_alpha_dict.get(c))
+    result += ' ' + str(CYRRILYC_ALPHA_DICT.get(c))
 print(result)
