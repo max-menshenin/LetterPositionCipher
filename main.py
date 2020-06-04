38e89cdcd59cf4bd57d7c7ce8dc01ae4ebe54f0e
postmind <postmind24@gmail.com>
2020-06-04 19:15:19 +0300

    remastered fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ text = 'варенник'
 CYRRILYC_ALPHA_DICT = dict(enumerate(CYRRILYC_ALPHA, 1))
 CYRRILYC_ALPHA_DICT = {v: k for k, v in CYRRILYC_ALPHA_DICT.items()}
 
-result = ' '
+result = ''
 for c in text:
     result += ' ' + str(CYRRILYC_ALPHA_DICT.get(c))
 print(result)
