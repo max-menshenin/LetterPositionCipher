53e42b59a7fd14f85ea95d49d655e88102d7c5a5
postmind <max.menshenin.dev@yandex.ru>
2020-10-11 12:03:47 +0300

    Ввод с клавиатуры

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 CYRRILYC_ALPHA = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-text = 'варенник'
+input_raw = input("Введите текст: " )
 
 CYRRILYC_ALPHA_DICT = dict(enumerate(CYRRILYC_ALPHA, 1))
 CYRRILYC_ALPHA_DICT = {v: k for k, v in CYRRILYC_ALPHA_DICT.items()}
 
 result = ''
-for c in text:
-    result += ''.join(str(CYRRILYC_ALPHA_DICT.get(c)))+' '
+for c in input_raw:
+    result += ''.join(str(CYRRILYC_ALPHA_DICT.get(c))) + ' '
 print(result)
